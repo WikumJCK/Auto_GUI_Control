6bca2801525bb784b7be852c7ab9ce2017283221
Wikum Jayasanka <wikumjck@gmail.com>
2021-03-26 12:32:21 +0530

    manual bootloading code

--- GUI_Automation.py
+++ GUI_Automation.py
@@ -1,8 +1,22 @@
-import pyautogui
+#----------Automatic arduino bootloader------
+#-------------------WikumJCK-----------------
 
-#print(pyautogui.position())
-for i in range(2):
-    pyautogui.click(3342,439)
-    pyautogui.click(3350,760)
 
-    print(pyautogui.confirm("Message"))
+#To get positions of the button run this code only
+#
+import pyautogui #importing pyautogui library
+
+BootLoadedICs = 1 #Variable to store Bootloaded ICs
+
+while True:
+    pyautogui.click(3342,439) # Location of "Tools" button
+    pyautogui.click(3350,760) # Location of "Burn Bootloader" button
+
+    Status = pyautogui.confirm("Bootloaded ICs : "+str(BootLoadedICs))#To manually continue to next IC
+
+    if Status =="OK": #Check Wheather the ICs programmes correctly or not
+        BootLoadedICs+=1
+    else:
+        continue
+
+    
